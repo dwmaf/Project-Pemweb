25a209521dcc3978227b00bc4a366f620c7c31be
nana72848e <d1041211005@student.untan.ac.id>
2024-10-12 18:53:48 +0700

    db info with .env

--- neon_connection.php
+++ neon_connection.php
@@ -1,8 +1,24 @@
 <?php
-$conn = pg_connect("postgresql://pemweb_owner:NqwDdb7QH8Kv@ep-weathered-leaf-a1at0cot-pooler.ap-southeast-1.aws.neon.tech/pemweb?sslmode=require");
+
+require 'vendor/autoload.php'; 
+
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv->load();
+
+$db_connection = $_ENV['DB_CONNECTION'];
+$db_host = $_ENV['DB_HOST'];
+$db_port = $_ENV['DB_PORT'];
+$db_name = $_ENV['DB_DATABASE'];
+$db_username = $_ENV['DB_USERNAME'];
+$db_password = $_ENV['DB_PASSWORD'];
+$db_sslmode = $_ENV['DB_SSLMODE'];
+
+$conn_string = "$db_connection://$db_username:$db_password@$db_host:$db_port/$db_name?sslmode=$db_sslmode";
+
+// Connect to PostgreSQL
+$conn = pg_connect($conn_string);
 
 if (!$conn) {
     die("Koneksi gagal: " . pg_last_error());
 }
 echo "Koneksi berhasil!";
-?>
